a148195e384cdf37456d6a88da16c2b3ae3c57af
Janzen Go <janneiljanzen.go@gmail.com>
2025-03-21 05:46:51 +0800

    Deploy

--- src/components/CategorySelection.jsx
+++ src/components/CategorySelection.jsx
@@ -139,7 +139,7 @@ const CategorySelection = ({ onSelectCategory }) => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-400 mr-2 flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span className="font-[Orbitron] text-left">You'll have 30 seconds to answer each question</span>
+            <span className="font-[Orbitron] text-left">You'll have 60 seconds to answer each question</span>
           </li>
           <li className="flex items-start">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-400 mr-2 flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
